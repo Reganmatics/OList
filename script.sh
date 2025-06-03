8841cf9b98df9484bfcc57e2abe880b3edf9d329
Reganmatics <devreganmatics@gmail.com>
2025-06-03 07:46:44 +0100

    update: modify script to install docker, docker-compose

--- script.sh
+++ script.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 #echo "dev" | sudo -S docker-compose up
+sudo apt update && sudo apt install -y \
+  python3-pip \
+  python3-venv \
+  unzip \
+  curl \
+  docker-compose \
+  docker.io \
+ # Create a virtual environment
+#python3 -m venv venv
+source venv/bin/activate
 
+# Install dbt and dbt-postgres
 pip3 install dbt-core dbt-postgres
 
-curl -L -o ./brazilian-e-commerce-company-olist.zip\
-  https://www.kaggle.com/api/v1/datasets/download/erak1006/brazilian-e-commerce-company-olist
+sudo docker-compose down
 
-unzip brazilian-e-commerce-company-olist.zip && rm brazilian-e-commerce-company-olist.zip
+sudo docker-compose up -d
+
+# Download the Olist dataset from Kaggle
+#curl -L -o ./brazilian-e-commerce-company-olist.zip\
+#  https://www.kaggle.com/api/v1/datasets/download/erak1006/brazilian-e-commerce-company-olist
+
+#unzip brazilian-e-commerce-company-olist.zip && rm brazilian-e-commerce-company-olist.zip
 
 # Move CSV files to a data directory
-mkdir data && mv *.csv ./data
+#mkdir data && mv *.csv ./data
